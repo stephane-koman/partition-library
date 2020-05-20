7c5a5a87dd3bee8f53ffb90b5baed36c71c8ac28
Stephane Koman <stefchris2@gmail.com>
2020-05-20 16:33:23 +0200

    Implementation de la fonction partition

--- src/main/java/com/adneom/stephanekoman/Library.java
+++ src/main/java/com/adneom/stephanekoman/Library.java
@@ -3,8 +3,31 @@
  */
 package com.adneom.stephanekoman;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
+
+    // Fonction de partionnement
+    public List<List<Integer>> partition(List<Integer> liste, int taille){
+        List<List<Integer>> listOfLists = new ArrayList<>();
+        try{
+            if(liste.size() > 0 && taille > 0){
+                if(liste.size() <= taille){
+                    listOfLists.add(liste);
+                }else{
+                    int t = (int) Math.floor((double) liste.size()/taille);
+                    for(int i = 0; i <= t; i++){
+                        int tmp = i * taille;
+                        List<Integer> sublist = liste.subList(tmp, Math.min(tmp + taille, liste.size()));
+                        if(sublist.size() > 0) listOfLists.add(sublist);
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return listOfLists;
     }
+
 }
